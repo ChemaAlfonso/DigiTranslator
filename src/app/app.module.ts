a2f84b14fc0ff251918e20b02fe0d8ebfe846c03
Chema Alfonso <jose.manuelchema111@gmail.com>
2020-06-06 22:34:40 +0200

    base working webapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { TraductorComponent } from './components/traductor/traductor.component';
+import { TraductorPipe } from './pipes/traductor.pipe';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TraductorComponent,
+    TraductorPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/traductor/traductor.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-traductor',
+  templateUrl: './traductor.component.html',
+  styleUrls: ['./traductor.component.scss']
+})
+export class TraductorComponent implements OnInit {
+
+  textoBase = '';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
