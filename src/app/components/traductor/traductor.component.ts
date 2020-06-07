d24fcc960057707adbceec381f0143418bd24dff
Chema Alfonso <jose.manuelchema111@gmail.com>
2020-06-07 12:00:24 +0200

    copy/paste & github icon  added

--- src/app/components/traductor/traductor.component.ts
+++ src/app/components/traductor/traductor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-traductor',
@@ -7,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TraductorComponent implements OnInit {
 
+  @ViewChild( 'textoTrad' ) textoTrad: ElementRef;
+
   textoBase = '';
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  copyToClipboard(): void {
+    this.textoTrad.nativeElement.select();
+    this.textoTrad.nativeElement.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    document.execCommand("copy");
+  }
+
+  async pasteFromClipboard(): Promise<any> {
+    const text = await navigator.clipboard.readText();
+    this.textoBase = text;
   }
 }
